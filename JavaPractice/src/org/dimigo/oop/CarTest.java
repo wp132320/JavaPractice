e102cf9a1afeafb758e88b6b700399374d06f480
puts Hello, <a>
2015-04-15 14:47:40 +0900

    7번째 과제입니다.

--- JavaPractice/src/org/dimigo/oop/CarTest.java
+++ JavaPractice/src/org/dimigo/oop/CarTest.java
@@ -47,14 +47,24 @@ public class CarTest {
 //			System.out.println("가격 : " + String.format("%,d", car[i].getPrice()) + "원");
 //			System.out.println();
 //		}
-		Car2 car2[] = {new Car2("현대자동차", "제네시스", "검정색", 225, 50000000), new Car2("기아자동차", "K7", "흰색", 246, 40000000), new Car2("삼성자동차", "SM7", "회색", 200, 38000000)};
+//		Car2 car2[] = {new Car2("현대자동차", "제네시스", "검정색", 225, 50000000), new Car2("기아자동차", "K7", "흰색", 246, 40000000), new Car2("삼성자동차", "SM7", "회색", 200, 38000000)};
+//		System.out.println("<< 자동차 목록 >>");
+//		for(int i=0; i<3; i++) {
+//			System.out.println("제조사명 : " + car2[i].getCompany());
+//			System.out.println("모델명 : " + car2[i].getModel());
+//			System.out.println("색상 : " + car2[i].getColor());
+//			System.out.println("최대속도 : " + car2[i].getMaxSpeed() + "km");
+//			System.out.println("가격 : " + String.format("%,d", car2[i].getPrice()) + "원");
+//			System.out.println();
+//		}
+		Car3 car3[] = {new Car3("현대자동차", "제네시스", "검정색", 225, 50000000), new Car3("기아자동차", "K7", "흰색", 246), new Car3("삼성자동차", "SM7", "회색")};
 		System.out.println("<< 자동차 목록 >>");
 		for(int i=0; i<3; i++) {
-			System.out.println("제조사명 : " + car2[i].getCompany());
-			System.out.println("모델명 : " + car2[i].getModel());
-			System.out.println("색상 : " + car2[i].getColor());
-			System.out.println("최대속도 : " + car2[i].getMaxSpeed() + "km");
-			System.out.println("가격 : " + String.format("%,d", car2[i].getPrice()) + "원");
+			System.out.println("제조사명 : " + car3[i].getCompany());
+			System.out.println("모델명 : " + car3[i].getModel());
+			System.out.println("색상 : " + car3[i].getColor());
+			System.out.println("최대속도 : " + car3[i].getMaxSpeed() + "km");
+			System.out.println("가격 : " + String.format("%,d", car3[i].getPrice()) + "원");
 			System.out.println();
 		}
 	}

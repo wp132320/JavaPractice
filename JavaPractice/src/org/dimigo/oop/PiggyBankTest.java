50ab878b5489f6019640f30f8d3ef2f08aa84f32
puts Hello, <a>
2015-06-16 11:49:51 +0900

    12번째 과제입니다.

---
+++ JavaPractice/src/org/dimigo/oop/PiggyBankTest.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *     |_ PiggyBankTest
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 6. 16.
+ * </pre>
+ *
+ * @author	: 오준용
+ * @version 	: 1.0
+ */
+public class PiggyBankTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FamilyMember father = new FamilyMember("아빠");
+		FamilyMember mother = new FamilyMember("엄마");
+		FamilyMember i = new FamilyMember("나");
+		FamilyMember brother = new FamilyMember("남동생");
+		FamilyMember.printMemberCnt();
+		PiggyBank.putMoney(father, 10000);
+		PiggyBank.putMoney(mother, 5000);
+		PiggyBank.putMoney(i, 2000);
+		PiggyBank.putMoney(brother, 1000);
+		PiggyBank.printBalance();
+		PiggyBank.putMoney(i, 1000);
+		PiggyBank.printBalance();
+	}
+
+}

378cd37fabb0c526616a49687dd31f334f99f1b9
puts Hello, <a>
2015-09-23 12:44:38 +0900

    16번째 과제입니다.

---
+++ JavaPractice/src/org/dimigo/exception/MovieTest.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package org.dimigo.exception;
+
+/**
+ * <pre>
+ * org.dimigo.exception
+ *     |_ MovieTest
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 9. 23.
+ * </pre>
+ *
+ * @author	: 오준용
+ * @version 	: 1.0
+ */
+public class MovieTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			Movie[] movies = {
+					new Movie("엔트맨", 12),
+					new Movie("사도", 12),
+					new Movie("베테랑", 15)
+			};
+			int age = 13;
+			for(Movie movie : movies) {
+				buyTicket(movie, age);
+			}
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	private static void buyTicket(Movie movie, int age) throws AgeCheckException{
+		if(age >= movie.getLimitAge()) {
+			System.out.println(movie.getTitle()+" 영화 즐감하세용~~");
+		}
+		else {
+			throw new AgeCheckException(movie);
+		}
+	}
+
+}
